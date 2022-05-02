5defd15889e68feb01240694012de92eb89842ee
rtfcv <rtfcv@localhost>
2022-05-02 23:50:42 +0900

    minor esthetic fix

--- init.sh
+++ init.sh
@@ -1,3 +1,4 @@
+# how to recreate this package dependency
 npm install react react-dom
 npm install --save-dev webpack webpack-cli copy-webpack-plugin html-webpack-plugin babel-loader @babel/core @babel/preset-env @babel/preset-react 
 # need babel as a loader for webpack
